96366f9ac1ab4c3165dc132ea5e980f7aaa6f60f
JLucGauvrit <gauvritj37@gmail.com>
2025-08-09 18:24:29 +0900

    Image compile

--- llama.cpp/build.sh
+++ llama.cpp/build.sh
@@ -2,4 +2,8 @@
 
 set -euo pipefail
 
-git clone https://github.com/ggml-org/llama.cpp && cd llama.cpp 
+git clone https://github.com/ggml-org/llama.cpp
+cd llama.cpp 
+
+cmake -B build
+cmake --build build --config Release
